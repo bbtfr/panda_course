7c6cdf839877cc03f6a7bbd9b2f52f34917c2aad
Theo <bbtfrr@gmail.com>
2014-09-20 21:26:12 +0800

    deploy scripts

---
+++ config/deploy.rb
@@ -0,0 +1,72 @@
+# config valid only for Capistrano 3.1
+lock '3.2.1'
+
+set :application, 'panda_course'
+set :repo_url, 'https://github.com/bbtfr/panda_course.git'
+# set :branch, 'deploy'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/var/www/panda_course'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+set :log_level, :debug
+# set :log_level, :info
+
+# Default value for :pty is false
+# set :pty, true
+
+# # Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/mobile}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+      invoke 'unicorn:restart'
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      within release_path do
+        rake 'tmp:clear'
+      end
+    end
+  end
+
+  desc "Update mobile files"
+  task :update_mobile do
+    run_locally do
+      within "mobile" do
+        execute :middleman, :build
+        execute :middleman, :deploy
+      end
+    end
+  end
+
+  after :publishing, :update_mobile
+
+end
