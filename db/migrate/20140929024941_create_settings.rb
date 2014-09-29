5e2dbfafe9919870c93823cb4533b0292778a8ab
Theo <bbtfrr@gmail.com>
2014-09-29 10:55:30 +0800

    default secret key

--- db/migrate/20140929024941_create_settings.rb
+++ db/migrate/20140929024941_create_settings.rb
@@ -9,6 +9,8 @@ class CreateSettings < ActiveRecord::Migration
     end
     
     add_index :settings, [ :thing_type, :thing_id, :var ], :unique => true
+
+    Setting.secret_key_base = 'a3738a6a2d4b0349976a19a3ab9307ec7b8d164fcd67b63b76faaada218d355d913a85f1da465f81fc08568771282f510c8fb55c7781ab290a1904ac19afb312'
   end
 
   def self.down
