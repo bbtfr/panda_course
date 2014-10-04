52a6de300aac677b5aff42d0843e7c3c6bef921f
Theo <bbtfrr@gmail.com>
2014-10-04 23:43:45 +0800

    excel & csv support

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -1,7 +1,29 @@
 class Admin::RegistrationsController < Admin::ApplicationController
+  require 'csv'
 
   def index
     @registrations = Registration.all
+
+    @attributes = %i(id full_name affiliation profession title address postcode phone email payment_type invoice_title created_at)
+    @listed_attributes = %i(full_name profession title phone email payment_type)
+    @detailed_attributes = @attributes
+    @translate_attributes = %i(profession title payment_type)
+    @data = Hash[ @registrations.map do |registration|
+      [ registration.id, Hash[ @attributes.map do |attribute|
+        attr_value = registration.send(attribute)
+        [ attribute, @translate_attributes.include?(attribute) ? attr_value.blank? ? nil : t(attr_value, scope: "activerecord.attributes.registration/#{attribute}", default: attr_value) : attr_value ]
+      end ] ]
+    end ]
+
+    respond_to do |format|
+      format.html
+      format.csv do
+        send_data(render_to_string)
+      end
+      format.xls do
+        send_data(render_to_string)
+      end
+    end
   end
 
 end
