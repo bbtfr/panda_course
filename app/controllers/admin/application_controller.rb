15d7edea867ac7bd898f81159718af3949630636
Theo <bbtfrr@gmail.com>
2014-10-09 11:01:17 +0800

    update content

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -9,7 +9,7 @@ class Admin::ApplicationController < ApplicationController
     def authenticate  
       authenticate_or_request_with_http_basic do |username, password| 
         username == USERNAME && password == PASSWORD  
-      end  
+      end
     end
 
     def set_locale
